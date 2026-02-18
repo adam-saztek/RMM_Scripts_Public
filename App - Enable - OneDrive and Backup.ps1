fe05a4df56fdcd496161e9972cd16355bfc47ccc
adam-saztek <adam.mcmillen@pisapain.com>
2026-02-17 17:06:29 -0700

    Add script to enable OneDrive for Windows 11
    Script checks for Windows 11 and applies registry settings to enable OneDrive if joined to a specific Azure tenant.

---
+++ App - Enable - OneDrive and Backup.ps1
@@ -0,0 +1,23 @@
+#Last changed here: 2024-12-19
+
+if ((Get-WmiObject Win32_OperatingSystem).Caption -Match "Windows 11") {
+    Write-Host "OS: Windows 11"
+    
+    $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo"
+    $guids = $subKey.GetSubKeyNames()
+    foreach($guid in $guids) {
+        $guidSubKey = $subKey.OpenSubKey($guid);
+        $tenantId = $guidSubKey.GetValue("TenantId");
+        #$userEmail = $guidSubKey.GetValue("UserEmail");
+    }
+
+    if ($tenantId -match 'YourAzureTenantID'){
+        Write-Host "STATUS: Joined to Azure Tenant"
+        Write-Host "STATUS: Applying registry entries to enable OneDrive"
+
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive" -Name "SilentAccountConfig" -PropertyType DWord -Value 1
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive" -Name "KFMSilentOptIn" -PropertyType String -Value "YourAzureTenantID"
+    }
+
+}
