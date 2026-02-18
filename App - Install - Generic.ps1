33a515a3e8028be4018b40469db4ec090e2ad19c
adam-saztek <adam.mcmillen@pisapain.com>
2026-02-17 18:54:40 -0700

    Add description comment for application installer
    Added a comment to describe the purpose of the script.

--- App - Install - Generic.ps1
+++ App - Install - Generic.ps1
@@ -1,4 +1,5 @@
 #Last changed here: 2025-08-22
+#Application installer.  Works with .msi files and .exes that have a silent install option.
 
 $applicationName = "Application Name"
 $installerFilename = "ApplicationInstaller.exe"
