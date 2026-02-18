c8491ff358760865e28c290f9857fabcad620bd7
adam-saztek <adam.mcmillen@pisapain.com>
2026-02-17 17:39:41 -0700

    Add files via upload

---
+++ System - Power Settings - Set.ps1
@@ -0,0 +1,13 @@
+#Last changed here: 2024-12-19
+
+if ((Get-WmiObject Win32_ComputerSystem).PartOfDomain)
+{
+    Write-Host "Workstation is on Active Directory, will not set power settings"
+}
+else
+{
+    Write-Host "Setting workstation to not sleep"
+    powercfg -change -standby-timeout-ac 0
+    Write-Host "Setting workstation to not shut off monitor for 15 minutes"
+    powercfg -change -monitor-timeout-ac 15
+}
