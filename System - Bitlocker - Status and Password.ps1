f7de6ce52de8a085ad1fdcab38c70ae847d98e7c
adam-saztek <adam.mcmillen@pisapain.com>
2026-02-17 17:36:32 -0700

    Add BitLocker status and recovery password script

---
+++ System - Bitlocker - Status and Password.ps1
@@ -0,0 +1,31 @@
+#Last changed here: 2025-03-07
+
+Import-Module $env:SyncroModule
+
+$BitlockerStatus = Get-BitLockerVolume -MountPoint C: | Select-Object VolumeStatus
+
+if ($BitlockerStatus.VolumeStatus -eq "FullyDecrypted")
+{
+    $BitlockerPassword = "Not Applicable"
+}
+else 
+{
+    $BitlockerPassword = (Get-BitLockerVolume -MountPoint C:).KeyProtector.RecoveryPassword
+}
+
+Set-Asset-Field -Name "BitLocker - Status" -Value $BitlockerStatus.VolumeStatus
+
+Set-Asset-Field -Name "BitLocker - C Drive Recovery Password" -Value $BitlockerPassword
+
+#Alert if laptop or remote desktop is not BitLocked
+if ($BitlockerStatus.VolumeStatus -ne "FullyEncrypted")
+{
+    if ((Get-Computerinfo).CsPCSystemType -eq 'mobile')
+    {
+        Rmm-Alert -Category 'Bitlocker' -Body 'BitLocker not enabled on laptop'
+    }
+    elseif ($env:computerName.StartsWith("REMOTE-"))
+    {
+        Rmm-Alert -Category 'Bitlocker' -Body 'BitLocker not enabled on remote computer (probably a desktop)'
+    }
+}
