2d41f7355232fa68698f145d29a5f29a8d5ca8bd
adam-saztek <adam.mcmillen@pisapain.com>
2026-02-17 19:26:27 -0700

    Add description to power settings script
    Added a description header for clarity on script functionality.

--- System - Power Settings - Set.ps1
+++ System - Power Settings - Set.ps1
@@ -1,4 +1,5 @@
 #Last changed here: 2024-12-19
+#Description: Sets workstations to not sleep and to turn off the monitor in 15 minutes
 
 if ((Get-WmiObject Win32_ComputerSystem).PartOfDomain)
 {
@@ -10,4 +11,5 @@ else
     powercfg -change -standby-timeout-ac 0
     Write-Host "Setting workstation to not shut off monitor for 15 minutes"
     powercfg -change -monitor-timeout-ac 15
+
 }
